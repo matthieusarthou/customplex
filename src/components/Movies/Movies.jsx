98e3fdc0e4b975d16745e01086377bf4a06d6763
unknown <matthieu.sarthou@gmail.com>
2023-04-03 17:51:25 +0400

    Movie view finished

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -2,11 +2,39 @@ import React, { useState, useEffect } from 'react';
 import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
 import { useSelector } from 'react-redux';
 import { useGetMoviesQuery } from '../../services/TMDB';
+import MovieList from '../MovieList/MovieList';
 
 const Movies = () => {
-  const { data } = useGetMoviesQuery();
-  console.log(data);
-  return <div>Movies</div>;
+  console.log('Movies');
+  const { data, error, isFetching } = useGetMoviesQuery();
+
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+  }
+
+  if (!data.results.length) {
+    return (
+      <Box display="flex" alignItems="center">
+        <Typography variant="h4">
+          No movies that match that name.
+          <br />
+          Please search for something else.
+        </Typography>
+      </Box>
+    );
+  }
+
+  if (error) return 'An error has occured.';
+
+  return (
+    <div>
+      <MovieList movies={data} />
+    </div>
+  );
 };
 
 export default Movies;
