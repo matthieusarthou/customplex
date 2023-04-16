593d87e55ab7d8e0a070232d1f2e72d4c6db0824
unknown <matthieu.sarthou@gmail.com>
2023-04-16 22:09:13 +0400

    fixed a typo in TMDB.js causing a 'insecure content loaded over HTTPS' bug

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,10 +1,38 @@
+import { ExitToApp } from '@mui/icons-material';
+import { Button, Typography, Box } from '@mui/material';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { userSelector } from '../../features/auth';
 
 const Profile = () => {
   const userData = useSelector(userSelector);
-  return <div>Profile {userData.user.username}</div>;
+  const favoriteMovies = [];
+  const logout = () => {
+    localStorage.clear();
+    window.location.href = '/';
+  };
+  return (
+    <Box>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+      >
+        <Typography
+          variant="h4"
+          gutterBottom
+        >
+          My Profile - {userData.user.username}
+        </Typography>
+        <Button
+          color="inherit"
+          onClick={logout}
+        >
+          Logout &nbsp; <ExitToApp />
+        </Button>
+      </Box>
+      {!favoriteMovies.length ? <Typography variant="h5">Add favorites or watchlist movies to see them here!</Typography> : <Box>Favorite movies</Box>}
+    </Box>
+  );
 };
 
 export default Profile;
